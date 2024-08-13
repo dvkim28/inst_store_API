b41ed896f888e793a9d19e6350f0ccb45e74e97a
dmitriy kim <d.villarionovich@gmail.com>
2024-08-13 20:51:35 +0200

    render preparing

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+
 import os
 from datetime import timedelta
 from pathlib import Path
@@ -30,7 +31,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["inst-store-api.onrender.com"]
 
 # Application definition
 
