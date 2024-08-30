2f08309074928634d9521beae3933a9c197dc37b
dmitriy kim <d.villarionovich@gmail.com>
2024-08-30 15:25:54 +0200

    Celery update

--- config/celery.py
+++ config/celery.py
@@ -1,9 +1,5 @@
-# celery.py
-
 from __future__ import absolute_import, unicode_literals
-
 import os
-
 from celery import Celery
 from django.conf import settings
 
@@ -14,6 +10,7 @@ app = Celery("config")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-app.autodiscover_tasks()
 
-CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
